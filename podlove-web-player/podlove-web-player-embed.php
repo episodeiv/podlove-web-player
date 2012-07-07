c19fcc47ffe561d5154eb5236ed1a4178f9b4d7f
Paul Golmann <master@pje.li>
2012-07-07 18:32:59 +0200

    initial embed player commit
    very early alpha/testing

---
+++ podlove-web-player/podlove-web-player-embed.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+
+<meta name="viewport" content="height=device-height, width=device-width">
+
+<title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
+
+<base href="<?php echo TEMPLATE_DIR_URL; ?>">
+
+<link rel="canonical" href="<?php echo get_permalink(); ?>">
+
+<link rel="stylesheet" href="mediaelement/mediaelementplayer.css">
+<link rel="stylesheet" href="podlove-web-player.css">
+<link rel="stylesheet" href="podlove-web-player-embed.css">
+
+<script src="libs/jquery-1.7.2.js"></script>
+<script src="mediaelement/mediaelement-and-player.min.js"></script>
+<script src="podlove-web-player.js"></script>
+<script src="podlove-web-player-embed.js"></script>
+
+<?php echo $mediahtml; ?>
+
+</html>
